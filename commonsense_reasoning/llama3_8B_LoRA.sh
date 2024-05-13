4d1255842b89cfe662fc4a874666c2beb4c19e7c
BootsofLagrangian <hard2251@yonsei.ac.kr>
2024-05-13 13:32:58 +0900

    fix typo

--- commonsense_reasoning/llama3_8B_LoRA.sh
+++ commonsense_reasoning/llama3_8B_LoRA.sh
@@ -28,12 +28,12 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file=./ddp.yaml  \
     --base_model $base_model \
     --data_path 'commonsense_170k.json' \
     --output_dir $output_dir \
-    --batch_size 16  --micro_batch_size 2 --num_epochs 3 \
+    --batch_size 16  --micro_batch_size 1 --num_epochs 3 \
     --learning_rate 2e-4 --cutoff_len 256 --val_set_size 120 \
     --eval_step 80 --save_step 80  --adapter_name $adapter_name \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
     --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing \
-    --wandb_project='seal-exp' --wandb_run_name='$base_model-'$adapter_name'-r'$1'-alpha'$2 \
+    --wandb_project='seal-exp' --wandb_run_name='$base_model'-'$adapter_name'-r'$1'-alpha'$2 \
 
 # python commonsense_evaluate.py \
 #     --model LLaMA-7B \
