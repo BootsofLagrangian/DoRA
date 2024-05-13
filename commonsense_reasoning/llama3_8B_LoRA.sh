820edc26ddbe5450b0b12afbb9c5f271a92d20c0
BootsofLagrangian <hard2251@yonsei.ac.kr>
2024-05-13 13:36:46 +0900

    fix typo

--- commonsense_reasoning/llama3_8B_LoRA.sh
+++ commonsense_reasoning/llama3_8B_LoRA.sh
@@ -33,7 +33,7 @@ NCCL_P2P_DISABLE=1 accelerate launch --config_file=./ddp.yaml  \
     --eval_step 80 --save_step 80  --adapter_name $adapter_name \
     --target_modules '["q_proj", "k_proj", "v_proj", "up_proj", "down_proj"]' \
     --lora_r $1 --lora_alpha $2 --use_gradient_checkpointing \
-    --wandb_project='seal-exp' --wandb_run_name='$base_model'-'$adapter_name'-r'$1'-alpha'$2 \
+    --wandb_project='seal-exp' --wandb_run_name='$base_model'-'$adapter_name'-r'$1'-alpha'$2' \
 
 # python commonsense_evaluate.py \
 #     --model LLaMA-7B \
